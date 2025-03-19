0b90c61f7047a5cf6c099b2350c84500d8d9ca99
Sergi <121488207+SergiMPorto@users.noreply.github.com>
2025-03-19 18:47:20 +0100

    uploading code

---
+++ NumeroPrimoAPP/src/test/java/Laboratorio/NumeroPrimoAPP/AppTest.java
@@ -0,0 +1,21 @@
+package Laboratorio.NumeroPrimoAPP;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testEsPrimo() {
+        assertTrue(true);
+    }
+
+
+}
